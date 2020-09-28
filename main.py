adea01f2b1407e0fb0084fa83f4247db863e0d41
PrinceAjudiya <18it004@charusat.edu.in>
2020-09-28 14:43:52 +0530

    Created main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 
-
 from PyQt5.QtWidgets import QApplication
 from view import GUI
+from controller import Controller
+from  model import evaluateExpression
+
 # Client code
 def main():
     """Main function."""
@@ -12,9 +14,11 @@ def main():
     view = GUI()
     view.show()
 
+    model = evaluateExpression
+
+    Controller(model=model, view=view)
+
     sys.exit(pycalc.exec_())
 
-
-    
 if __name__ == '__main__':
     main()
